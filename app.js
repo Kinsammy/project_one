05e3e39bc79d207929f14a2a0568a4dd7300d2c8
kinsammy <fanusamuel@gmail.com>
2022-06-01 17:35:18 +0100

    comment

---
+++ app.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+// Event Listener
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
